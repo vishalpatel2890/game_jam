694e16c6783e35c2216f888c201f15a85d589a52
Andrew Sproul <ajs05mfsu@gmail.com>
2019-06-22 21:02:47 -0400

    adding rotating green flask art

--- projectiles/projectile.py
+++ projectiles/projectile.py
@@ -1,11 +1,12 @@
 import pygame
 
-green_flask = [pygame.image.load('projectiles/images/erl_flask_GREEN1.png'), pygame.image.load('projectiles/images/erl_flask_GREEN2.png'), pygame.image.load('projectiles/images/erl_flask_GREEN3.png'), pygame.image.load('projectiles/images/erl_flask_GREEN4.png'), pygame.image.load('projectiles/images/erl_flask_GREEN5.png'), pygame.image.load('projectiles/images/erl_flask_GREEN6.png'), pygame.image.load('projectiles/images/erl_flask_GREEN7.png'), pygame.image.load('projectiles/images/erl_flask_GREEN8.png'), pygame.image.load('projectiles/images/erl_flask_GREEN9.png'), pygame.image.load('projectiles/images/erl_flask_GREEN10.png')]
+# green_flask = [pygame.image.load('projectiles/images/erl_flask_GREEN1.png'), pygame.image.load('projectiles/images/erl_flask_GREEN2.png'), pygame.image.load('projectiles/images/erl_flask_GREEN3.png'), pygame.image.load('projectiles/images/erl_flask_GREEN4.png'), pygame.image.load('projectiles/images/erl_flask_GREEN5.png'), pygame.image.load('projectiles/images/erl_flask_GREEN6.png'), pygame.image.load('projectiles/images/erl_flask_GREEN7.png'), pygame.image.load('projectiles/images/erl_flask_GREEN8.png'), pygame.image.load('projectiles/images/erl_flask_GREEN9.png'), pygame.image.load('projectiles/images/erl_flask_GREEN10.png')]
 
-# rename to red_flask afterwards --->
+green_flask = [pygame.image.load('projectiles/images/erl_flask_GREEN11.png'), pygame.image.load('projectiles/images/erl_flask_GREEN12.png'), pygame.image.load('projectiles/images/erl_flask_GREEN13.png'), pygame.image.load('projectiles/images/erl_flask_GREEN14.png'), pygame.image.load('projectiles/images/erl_flask_GREEN15.png'), pygame.image.load('projectiles/images/erl_flask_GREEN16.png'), pygame.image.load('projectiles/images/erl_flask_GREEN17.png'), pygame.image.load('projectiles/images/erl_flask_GREEN18.png'), pygame.image.load('projectiles/images/erl_flask_GREEN19.png'), pygame.image.load('projectiles/images/erl_flask_GREEN20.png'),pygame.image.load('projectiles/images/erl_flask_GREEN21.png'), pygame.image.load('projectiles/images/erl_flask_GREEN22.png'), pygame.image.load('projectiles/images/erl_flask_GREEN23.png'), pygame.image.load('projectiles/images/erl_flask_GREEN24.png'), pygame.image.load('projectiles/images/erl_flask_GREEN25.png'), pygame.image.load('projectiles/images/erl_flask_GREEN26.png'), pygame.image.load('projectiles/images/erl_flask_GREEN27.png'), pygame.image.load('projectiles/images/erl_flask_GREEN28.png'), pygame.image.load('projectiles/images/erl_flask_GREEN29.png'), pygame.image.load('projectiles/images/erl_flask_GREEN30.png')]
+
 red_flask = [pygame.image.load('projectiles/images/erl_flask_RED1.png'), pygame.image.load('projectiles/images/erl_flask_RED2.png'), pygame.image.load('projectiles/images/erl_flask_RED3.png'), pygame.image.load('projectiles/images/erl_flask_RED4.png'), pygame.image.load('projectiles/images/erl_flask_RED5.png'), pygame.image.load('projectiles/images/erl_flask_RED6.png'), pygame.image.load('projectiles/images/erl_flask_RED7.png'), pygame.image.load('projectiles/images/erl_flask_RED8.png'), pygame.image.load('projectiles/images/erl_flask_RED9.png'), pygame.image.load('projectiles/images/erl_flask_RED10.png')]
 #
-purple_flask_ = [pygame.image.load('projectiles/images/erl_flask_PURPLE1.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE2.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE3.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE4.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE5.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE6.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE7.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE8.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE9.png'), pygame.image.load('projectiles/images/erl_flask_PURPLE10.png')]
+purple_flask_ = [pygame.image.load('projectiles/images/erl_flask_PURP1.png'), pygame.image.load('projectiles/images/erl_flask_PURP2.png'), pygame.image.load('projectiles/images/erl_flask_PURP3.png'), pygame.image.load('projectiles/images/erl_flask_PURP4.png'), pygame.image.load('projectiles/images/erl_flask_PURP5.png'), pygame.image.load('projectiles/images/erl_flask_PURP6.png'), pygame.image.load('projectiles/images/erl_flask_PURP7.png'), pygame.image.load('projectiles/images/erl_flask_PURP8.png'), pygame.image.load('projectiles/images/erl_flask_PURP9.png'), pygame.image.load('projectiles/images/erl_flask_PURP10.png')]
 
 
 
@@ -20,23 +21,11 @@ class projectile(object):
 
     def draw(self, win):
         """Blit the erl_flask to the window."""
-        if self.flask_bubbles + 1 >= 30:
+        if self.flask_bubbles + 1 >= 60:
             self.flask_bubbles = 0
         win.blit(pygame.transform.scale(green_flask[self.flask_bubbles//3], (32,32)), (self.x,self.y-12))
         self.flask_bubbles += 1
 
-        # win.blit(pygame.transform.scale(green_flask[0], (36,36)), (self.x,self.y-12))
-
-
-
-
-        # if not(self.standing):
-        #     if self.left:
-        #         win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
-        #         self.walkCount += 1
-        #     elif self.right:
-        #         win.blit(walkRight[self.walkCount//3], (self.x,self.y))
-        #         self.walkCount +=1
 
 
 
